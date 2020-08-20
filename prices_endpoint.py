c1b2aedabdc0b68d1d1d932644102c5eca96d577
Anton "TonyL" Lysakov <tlysakov@gmail.com>
2020-08-20 21:42:37 +0700

    endpoint template

---
+++ prices_endpoint.py
@@ -0,0 +1,18 @@
+import flask
+import json
+from flask import jsonify, request
+import logging
+
+app = flask.Flask(__name__)
+
+
+@app.route('/api/v1/summary', methods=['GET'])
+def summary():
+    with open('summary.json') as json_file:
+        data = json.load(json_file)
+    return jsonify(data)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(filename='/var/log/dex_prices_endpoint.log',level=logging.DEBUG)
+    app.run(host='0.0.0.0', port=8080)
