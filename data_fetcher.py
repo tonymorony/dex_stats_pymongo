59c26504b4ce9fc61ffbfbd892fe3af0102de457
Anton "TonyL" Lysakov <tlysakov@gmail.com>
2020-08-20 22:22:10 +0700

    summary function

--- data_fetcher.py
+++ data_fetcher.py
@@ -11,17 +11,16 @@ adex_tickers = ["AWC", "AXE", "BAT", "BCH", "BET", "BOTS", "BTC", "BUSD", "CCL",
 possible_pairs = list(itertools.permutations(adex_tickers, 2))
 db_connection = MongoAPI()
 
-pairs_iter = 0
-swaps_iter = 0
-summary_endpoint_data = []
-for pair in possible_pairs:
-    total_swaps = len(list(db_connection.find_swaps_for_market(pair[0], pair[1])))
-    # fetching data for pairs with historical swaps only
-    if total_swaps > 0:
-        pair_data = {}
-        pair_data["trading_pair"] = pair[0] + "_" + pair[1]
-        summary_endpoint_data.append(pair_data)
+def fetch_summary_data():
+    summary_endpoint_data = []
+    for pair in possible_pairs:
+        total_swaps = len(list(db_connection.find_swaps_for_market(pair[0], pair[1])))
+        # fetching data for pairs with historical swaps only
+        if total_swaps > 0:
+            pair_data = {}
+            pair_data["trading_pair"] = pair[0] + "_" + pair[1]
+            summary_endpoint_data.append(pair_data)
 
-with open('summary.json', 'w') as f:
-    json.dump(summary_endpoint_data, f)
-    
+    with open('summary.json', 'w') as f:
+        json.dump(summary_endpoint_data, f)
+        
