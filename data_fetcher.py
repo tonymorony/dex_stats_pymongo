61a485f93113ad44a8cf1356e62787eda227385f
Anton "TonyL" Lysakov <tlysakov@gmail.com>
2020-08-20 21:48:03 +0700

    fetch into json

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,4 +1,5 @@
 import itertools
+import json
 from db_connector import MongoAPI
 
 adex_tickers = ["AWC", "AXE", "BAT", "BCH", "BET", "BOTS", "BTC", "BUSD", "CCL", "CHIPS", "CRYPTO", "DAI", "DASH",
@@ -8,17 +9,19 @@ adex_tickers = ["AWC", "AXE", "BAT", "BCH", "BET", "BOTS", "BTC", "BUSD", "CCL",
 
 # 45 tickers atm = 1980 pairs
 possible_pairs = list(itertools.permutations(adex_tickers, 2))
-
 db_connection = MongoAPI()
 
 pairs_iter = 0
 swaps_iter = 0
+summary_endpoint_data = []
 for pair in possible_pairs:
     total_swaps = len(list(db_connection.find_swaps_for_market(pair[0], pair[1])))
+    # fetching data for pairs with historical swaps only
     if total_swaps > 0:
-        print("Pair: " + str(pair) + " Total swaps: " + str(total_swaps))
-        pairs_iter += 1
-        swaps_iter += total_swaps
-print("Total swaps directions: " + str(pairs_iter))
-print("Total swaps: " + str(swaps_iter))
+        pair_data = {}
+        pair_data["trading_pair"] = pair[0] + "_" + pair[1]
+        summary_endpoint_data.append(pair_data)
+
+with open('summary.json', 'w') as f:
+    json.dump(summary_endpoint_data, f)
     
