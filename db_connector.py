b7d7c5bac811ca3ac2d6f0cecbd0278ab9a19432
Anton "TonyL" Lysakov <tlysakov@gmail.com>
2020-08-27 19:20:55 +0700

    Update db_connector.py

--- db_connector.py
+++ db_connector.py
@@ -5,32 +5,29 @@ from datetime import datetime, timedelta
 
 
 class MongoAPI:
-    def __init__(self, collection):
+    def __init__(self, collection_name):
         self.client = MongoClient("mongodb://localhost:27017/")
-        self.swaps = self.client.swaps
-        if 'failed' in collection:
-            self.failed_swaps_collection = self.swaps.failed
-        if 'successful' in collection:
-            self.successful_swaps_collection = self.swaps.successful
-        
+        self.swaps = self.client.swaps.collection_name
+
 
     def find_swap_by_uuid(self, uuid):
         query = {"uuid": uuid}
-        result = self.successful_swaps_collection.find(query)
+        result = self.self.swaps.find(query)
         return result
 
 
     def find_swaps_since_timestamp(self, timestamp):
         query = {"events.event.data.started_at": {"$gt": timestamp}}
-        result = self.successful_swaps_collection.find(query)
+        result = self.self.swaps.find(query)
         return result
 
 
     def find_swaps_for_market(self, maker_coin, taker_coin):
        query = {"$and": [{"maker_coin": maker_coin}, {"taker_coin": taker_coin}]}
-       result = self.successful_swaps_collection.find(query)
+       result = self.self.swaps.find(query)
        return result
 
 
     def find_successful_swap_pairs(self):
         pass
+    
