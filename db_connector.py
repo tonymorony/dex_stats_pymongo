2ce90b6fd6d7e7cc45122f456f3a318038e6f325
dathbezumniy <gdath100500@gmail.com>
2020-08-27 19:09:46 +0700

    fix parse_db events, and db_connector

--- db_connector.py
+++ db_connector.py
@@ -2,27 +2,35 @@ from pymongo import MongoClient
 import os
 import json
 from datetime import datetime, timedelta
-from pprint import pprint
+
 
 class MongoAPI:
-    def __init__(self):
+    def __init__(self, collection):
         self.client = MongoClient("mongodb://localhost:27017/")
-        self.db = self.client["swaps"]
-        self.swaps_collection = self.db.swaps
+        self.swaps = self.client.swaps
+        if 'failed' in collection:
+            self.failed_swaps_collection = self.swaps.failed
+        if 'successful' in collection:
+            self.successful_swaps_collection = self.swaps.successful
+        
 
     def find_swap_by_uuid(self, uuid):
         query = {"uuid": uuid}
-        result = self.swaps_collection.find(query)
+        result = self.successful_swaps_collection.find(query)
         return result
 
 
     def find_swaps_since_timestamp(self, timestamp):
         query = {"events.event.data.started_at": {"$gt": timestamp}}
-        result = self.swaps_collection.find(query)
+        result = self.successful_swaps_collection.find(query)
         return result
 
 
     def find_swaps_for_market(self, maker_coin, taker_coin):
        query = {"$and": [{"maker_coin": maker_coin}, {"taker_coin": taker_coin}]}
-       result = self.swaps_collection.find(query)
+       result = self.successful_swaps_collection.find(query)
        return result
+
+
+    def find_successful_swap_pairs(self):
+        pass
--- parse_swaps_db.py
+++ parse_swaps_db.py
@@ -5,7 +5,7 @@ import sys
 
 client = MongoClient('mongodb://localhost:27017/')
 
-swaps_db = client.swaps
+swaps_db = client.swaps_db
 
 db_successful_swaps = swaps_db.successful
 db_failed_swaps = swaps_db.failed
@@ -16,6 +16,7 @@ taker_folder_path = "SWAPS/STATS/TAKER/"
 
 maker_files_list = [pos_json for pos_json in os.listdir(maker_folder_path) if pos_json.endswith('.json')]
 taker_files_list = [pos_json for pos_json in os.listdir(taker_folder_path) if pos_json.endswith('.json')]
+    
 
 
 # TODO: probably have to combine maker/taker data for uuid into single document (however not sure if it needed)
@@ -23,20 +24,23 @@ for maker_file in maker_files_list:
     with open(maker_folder_path + maker_file) as f:
         data = json.load(f)
         swap_events = [ x['event']['type'] for x in data['events'] ]
-        if 'Failed' in swap_events:
-            db_failed_swaps.insert(data)
-        else:
-            db_successful_swaps.insert(data)
+        for event in swap_events:
+            if 'Failed' in event:
+                db_failed_swaps.insert(data)
+            else:
+                db_successful_swaps.insert(data)
+            
 
 
 for taker_file in taker_files_list:
     with open(taker_folder_path + taker_file) as f:
         data = json.load(f)
         swap_events = [ x['event']['type'] for x in data['events'] ]
-        if 'Failed' in swap_events:
-            db_failed_swaps.insert(data)
-        else:
-            db_successful_swaps.update({"uuid": data["uuid"]}, data, upsert=True)
+        for event in swap_events:
+            if 'Failed' in event:
+                db_failed_swaps.insert(data)
+            else:
+                db_successful_swaps.update({"uuid": data["uuid"]}, data, upsert=True)
 
 
 resp = swaps_db.create_index([ ("uuid", 1) ])
