9f727b68dbe2e39dbe8e24674664fe2867255c2d
Anton "TonyL" Lysakov <tlysakov@gmail.com>
2020-08-27 15:00:12 +0700

    added orderbook rpc call

--- prices_endpoint.py
+++ prices_endpoint.py
@@ -5,7 +5,6 @@ import logging
 
 app = flask.Flask(__name__)
 
-
 @app.route('/api/v1/summary', methods=['GET'])
 def summary():
     with open('summary.json') as json_file:
@@ -18,6 +17,17 @@ def ticker():
         data = json.load(json_file)
     return jsonify(data)
 
+@app.route('/api/v1/orderbook/<market_pair>', methods=['GET'])
+def orderbook(market_pair="KMD_BTC"):
+    with open('orderbook_data.json') as json_file:
+        #TODO: handle non existent pair
+        pairs_data = json.load(json_file)
+        for pair in pairs_data:
+            if market_pair in pair.keys():
+                data = pair
+    return jsonify(data)
+
 if __name__ == '__main__':
-    logging.basicConfig(filename='/var/log/dex_prices_endpoint.log',level=logging.DEBUG)
+    #logging.basicConfig(filename='/var/log/dex_prices_endpoint.log',level=logging.DEBUG)
     app.run(host='0.0.0.0', port=8080)
+    
