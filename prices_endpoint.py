a18d84156ac7797a76fdc4c933cac36de926b34f
Anton "TonyL" Lysakov <tlysakov@gmail.com>
2020-08-27 13:39:08 +0700

    ticker endpoint rpc call

--- prices_endpoint.py
+++ prices_endpoint.py
@@ -12,6 +12,11 @@ def summary():
         data = json.load(json_file)
     return jsonify(data)
 
+@app.route('/api/v1/ticker', methods=['GET'])
+def summary():
+    with open('ticker.jsonn') as json_file:
+        data = json.load(json_file)
+    return jsonify(data)
 
 if __name__ == '__main__':
     logging.basicConfig(filename='/var/log/dex_prices_endpoint.log',level=logging.DEBUG)
