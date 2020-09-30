5b73fa01d2ccdbd55ac154323ec19842fa5f6587
Anton "TonyL" Lysakov <tlysakov@gmail.com>
2020-09-30 11:44:47 +0700

    not parsing taker folder
    it caused data duplication and all volumes doubling

--- db_parser.py
+++ db_parser.py
@@ -8,10 +8,10 @@ client = MongoClient('mongodb://localhost:27017/')
 db = client['swaps']
 
 maker_folder_path = "DB/<pubkey>/SWAPS/STATS/MAKER/"
-taker_folder_path = "DB/<pubkey>/SWAPS/STATS/TAKER/"
+#taker_folder_path = "DB/<pubkey>/SWAPS/STATS/TAKER/"
 
 maker_files_list = [pos_json for pos_json in os.listdir(maker_folder_path) if pos_json.endswith('.json')]
-taker_files_list = [pos_json for pos_json in os.listdir(taker_folder_path) if pos_json.endswith('.json')]
+#taker_files_list = [pos_json for pos_json in os.listdir(taker_folder_path) if pos_json.endswith('.json')]
 
 is_fresh_db = db["swaps"].count_documents({}) < 1
 
@@ -25,13 +25,13 @@ for file in maker_files_list:
         else:
             db["swaps"].update({"uuid": data["uuid"]}, data, upsert=True)
 
-for file in taker_files_list:
-    with open(taker_folder_path + file) as f:
-        data = json.load(f)
-        if is_fresh_db:
-            db["swaps"].insert(data)
-        else:
-            db["swaps"].update({"uuid": data["uuid"]}, data, upsert=True)
+# for file in taker_files_list:
+#     with open(taker_folder_path + file) as f:
+#         data = json.load(f)
+#         if is_fresh_db:
+#             db["swaps"].insert(data)
+#         else:
+#             db["swaps"].update({"uuid": data["uuid"]}, data, upsert=True)
 
 if is_fresh_db:
     resp = db["swaps"].create_index([ ("uuid", 1) ])
