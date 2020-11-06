be81f815a4d48773d8aa2006b3b324f269cef457
Anton "TonyL" Lysakov <tlysakov@gmail.com>
2020-11-06 10:57:01 +0700

    stress_test api call

--- app/main.py
+++ app/main.py
@@ -4,36 +4,41 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
-@app.get('/api/v1/summary')
+@app.get('/api/v1/leaderboard')
 async def summary():
-    with open('data/summary.json') as f:
+    with open('data/stress_test.json') as f:
         summary = json.load(f)
     return summary
 
-
-@app.get('/api/v1/ticker')
-async def ticker():
-    with open('data/ticker.json') as f:
-        ticker = json.load(f)
-    return ticker
-
-
-@app.get('/api/v1/orderbook/{market_pair}')
-async def orderbook(market_pair: str = "ALL"):
-    with open('data/orderbook.json') as f:
-        orderbook = json.load(f)
-        try:
-            return orderbook[market_pair]
-        except KeyError:
-            return {'error': 'no such pair'}
-
-
-@app.get('/api/v1/trades/{market_pair}')
-async def trades(market_pair: str = "ALL"):
-    with open('data/trades.json') as f:
-        trades = json.load(f)
-        try:
-            return trades[market_pair]
-        except KeyError:
-            return {'error': 'no such pair'}
+# @app.get('/api/v1/summary')
+# async def summary():
+#     with open('data/summary.json') as f:
+#         summary = json.load(f)
+#     return summary
+#
+#
+# @app.get('/api/v1/ticker')
+# async def ticker():
+#     with open('data/ticker.json') as f:
+#         ticker = json.load(f)
+#     return ticker
+#
+#
+# @app.get('/api/v1/orderbook/{market_pair}')
+# async def orderbook(market_pair: str = "ALL"):
+#     with open('data/orderbook.json') as f:
+#         orderbook = json.load(f)
+#         try:
+#             return orderbook[market_pair]
+#         except KeyError:
+#             return {'error': 'no such pair'}
+#
+#
+# @app.get('/api/v1/trades/{market_pair}')
+# async def trades(market_pair: str = "ALL"):
+#     with open('data/trades.json') as f:
+#         trades = json.load(f)
+#         try:
+#             return trades[market_pair]
+#         except KeyError:
+#             return {'error': 'no such pair'}
