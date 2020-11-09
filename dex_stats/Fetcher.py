35a7bfa3ee1d3a94f60a7c9ff45d1f279954eee6
Anton "TonyL" Lysakov <tlysakov@gmail.com>
2020-11-09 21:16:41 +0700

    graph data struct

--- dex_stats/Fetcher.py
+++ dex_stats/Fetcher.py
@@ -93,9 +93,9 @@ class Fetcher:
             json.dump(self.stress_test_summary, f)
         with open('../data/stress_test_uuids.json', 'w') as f:
             json.dump(self.stress_test_swaps_data, f)
-        graph_objects = [{k: v} for k, v in self.graph_data.items()]
+        #graph_objects = [{k: v} for k, v in self.graph_data.items()]
         with open('../data/graph_data.json', 'w') as f:
-            json.dump(graph_objects, f)
+            json.dump(self.graph_data, f)
         self.save_ticker_data_as_json()
         self.save_trades_data_as_json()
 
